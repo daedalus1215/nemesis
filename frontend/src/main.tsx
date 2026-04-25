4cda9ffeafacef5b3ab12e9c8a8752179b27655d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2026-04-25 08:23:47 -0400

    follow existing convention

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AuthProvider } from "./auth/AuthContext";
+import { AuthProvider } from "./auth/AuthContext.tsx";
 import { App } from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from './context/theme';
+import { theme } from './context/theme.tsx';
 
 const queryClient = new QueryClient();
 
